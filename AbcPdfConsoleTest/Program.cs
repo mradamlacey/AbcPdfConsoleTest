36a297a7223c92e23b85a02b2120f4f98f1929a3
Adam Lacey <adam.lacey@cbre.com>
2015-08-27 11:38:13 -0500

    Add readme ;

---
+++ AbcPdfConsoleTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebSupergoo.ABCpdf10;
+
+namespace AbcPdfConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello");
+
+            System.Console.Read();
+
+            Doc theDoc = new Doc();
+            theDoc.Rect.Inset(72, 144);
+            theDoc.HtmlOptions.AddLinks = true;
+
+            int theID;
+            theID = theDoc.AddImageUrl("http://www.yahoo.com/");
+
+            while (true)
+            {
+                theDoc.FrameRect();
+                if (!theDoc.Chainable(theID))
+                    break;
+                theDoc.Page = theDoc.AddPage();
+                theID = theDoc.AddImageToChain(theID);
+            }
+
+            for (int i = 1; i <= theDoc.PageCount; i++)
+            {
+                theDoc.PageNumber = i;
+                theDoc.Flatten();
+            }
+
+            theDoc.Save("c:\\src\\AbcPdfConsoleTest.pdf");
+            theDoc.Clear();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# `AbcPdfConsoleTest`
+
+Test program to demonstrate usage of the AbcPDF.net component from WebSuperGoo
+
+Demonstrates the following:
+- Render a PDF from a URL (`www.yahoo.com`)
+- Renders multiple pages in the PDF based on the content not fitting on a single page
+- Rendering all HTML hyperlink as 'live' so that a user can click on them in the PDF to open a web browser
